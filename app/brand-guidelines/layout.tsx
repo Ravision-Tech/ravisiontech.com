281556bacdeb6df76ce44a30b87a5eefc64dda13
Dylan Ravel <dylanm.ravel@gmail.com>
2026-05-13 10:20:00 -0700

    Add brand guidelines embed metadata

---
+++ app/brand-guidelines/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+
+import { Branding } from "@/lib/branding";
+import { DomainURL } from "@/lib/links";
+
+export const metadata: Metadata = {
+  title: "Ravision Tech – Brand Guidelines",
+  openGraph: {
+    type: "website",
+    url: `${DomainURL}/brand-guidelines`,
+    siteName: Branding.Name,
+    title: `${Branding.Name} – Brand Guidelines`,
+    images: [
+      {
+        url: "/branding/meta-images/brand-guidelines-social-preview-image.png",
+        alt: `${Branding.Name} – Brand Guidelines`,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: `${Branding.Name} – Brand Guidelines`,
+    images: [
+      {
+        url: "/branding/meta-images/brand-guidelines-social-preview-image.png",
+        alt: `${Branding.Name} – Brand Guidelines`,
+      },
+    ],
+  },
+};
+
+const BrandGuidelinesLayout = ({ children }: { children: React.ReactNode }) => {
+  return children;
+};
+
+export default BrandGuidelinesLayout;
